a46671d63b439e5f1d92606f448654e0a76e7bb4
João Soares <jsoaresgeral@gmail.com>
2012-09-23 17:08:40 +0100

    Change preconfiguration code to be generic
    Add preconfiguration file brx_config.rb to .gitignore
    
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

--- lib/travian.rb
+++ lib/travian.rb
@@ -21,8 +21,8 @@ module Travian
   end
 end
 
-# Travian.configure do |cfg|
-#   cfg.server = 'tx3.travian.com.br'
-#   cfg.user = 'jasoares'
-#   cfg.password = 'frohike'
-# end
+Travian.configure do |cfg|
+  cfg.server = 'tx3.travian.com.br'
+  cfg.user = 'user'
+  cfg.password = 'password'
+end
